dfafb347561ee1b6b9c4a39568ef729ec66dbc28
Unknown <218435@student.pwr.edu.pl>
2019-04-23 12:12:38 +0200

    Add savePlayer
    Post method to add new players

--- players/src/main/java/com/example/players/controller/PlayerControllerRest.java
+++ players/src/main/java/com/example/players/controller/PlayerControllerRest.java
@@ -5,11 +5,10 @@ import com.example.players.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -35,6 +34,18 @@ public class PlayerControllerRest {
         return playerRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> savePlayer(@RequestBody Player player){
+        Player save = playerRepository.save(player);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(save.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(save);
     }
 
 }
